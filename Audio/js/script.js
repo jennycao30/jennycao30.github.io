7b82b786aa53318e6b1a1c6950b96aef982de882
jennycao30 <60491765+jennycao30@users.noreply.github.com>
2020-09-15 13:01:06 -0400

    Add files via upload

---
+++ Audio/js/script.js
@@ -0,0 +1,249 @@
+const thumbnail = document.querySelector('#thumbnail'); // album cover 
+const song = document.querySelector('#song'); // audio object
+
+const songArtist = document.querySelector('.song-artist'); // element where track artist appears
+const songTitle = document.querySelector('.song-title'); // element where track title appears
+const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
+let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
+
+songIndex = 0;
+songs = ['../media/Ikson - Contigo (Official).wav', '../media/Ikson - Waves (Official).wav']; // object storing paths for audio objects
+thumbnails = ['../media/contigo.jpg', '../media/waves.jpg']; // object storing paths for album covers and backgrounds
+songArtists = ['Ikson', 'Ikson']; // object storing track artists
+songTitles = ["Ikson - Contigo (Official)", "Ikson - Waves (Official)"]; // object storing track titles
+
+function calculateTotalValue(length) {
+  var minutes = Math.floor(length / 60),
+    seconds_int = length - minutes * 60,
+    seconds_str = seconds_int.toString(),
+    seconds = seconds_str.substr(0, 2),
+    time = minutes + ':' + seconds
+
+  return time;
+}
+
+function calculateCurrentValue(currentTime) {
+  var current_hour = parseInt(currentTime / 3600) % 24,
+    current_minute = parseInt(currentTime / 60) % 60,
+    current_seconds_long = currentTime % 60,
+    current_seconds = current_seconds_long.toFixed(),
+    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+
+  return current_time;
+}
+
+function initProgressBar() {
+  var player = document.getElementById('player');
+  var length = player.duration
+  var current_time = player.currentTime;
+
+  // calculate total length of value
+  var totalLength = calculateTotalValue(length)
+  jQuery(".end-time").html(totalLength);
+
+  // calculate current value time
+  var currentTime = calculateCurrentValue(current_time);
+  jQuery(".start-time").html(currentTime);
+
+  var progressbar = document.getElementById('seekObj');
+  progressbar.value = (player.currentTime / player.duration);
+  progressbar.addEventListener("click", seek);
+
+  if (player.currentTime == player.duration) {
+    $('#play-btn').removeClass('pause');
+  }
+
+  function seek(evt) {
+    var percent = evt.offsetX / this.offsetWidth;
+    player.currentTime = percent * player.duration;
+    progressbar.value = percent / 100;
+  }
+};
+
+
+function initPlayers(num) {
+  // pass num in if there are multiple audio players e.g 'player' + i
+
+  for (var i = 0; i < num; i++) {
+    (function() {
+
+      // Variables
+      // ----------------------------------------------------------
+      // audio embed object
+      var playerContainer = document.getElementById('player-container'),
+        player = document.getElementById('player'),
+        isPlaying = false,
+        playBtn = document.getElementById('play-btn');
+
+      // Controls Listeners
+      // ----------------------------------------------------------
+      if (playBtn != null) {
+        playBtn.addEventListener('click', function() {
+          togglePlay()
+        });
+      }
+
+      // Controls & Sounds Methods
+      // ----------------------------------------------------------
+      function togglePlay() {
+        if (player.paused === false) {
+          player.pause();
+          isPlaying = false;
+          $('#play-btn').removeClass('pause');
+
+        } else {
+          player.play();
+          $('#play-btn').addClass('pause');
+          isPlaying = true;
+        }
+      }
+    }());
+  }
+}
+
+
+initPlayers(jQuery('#player-container').length);
+
+
+/*
+// function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track 
+function nextSong() {
+    songIndex++;
+    if (songIndex > 1) {
+        songIndex = 0;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+
+// function where songIndex is decremented, song/thumbnail image/background image/song artist/song title changes to previous index value, and playPause() runs to play previous track 
+function previousSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = 1;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+*/                             /*This was the js function for the forward and previous song button I was trying to work out*/
+
+
+
+/*const background = document.querySelector('#background'); // background derived from album cover below
+const thumbnail = document.querySelector('#thumbnail'); // album cover 
+const song = document.querySelector('#song'); // audio object
+
+const songArtist = document.querySelector('.song-artist'); // element where track artist appears
+const songTitle = document.querySelector('.song-title'); // element where track title appears
+const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
+let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
+
+songIndex = 0;
+songs = ['../media/Ikson - Contigo (Official).wav', '../media/Ikson - Waves (Official).wav']; // object storing paths for audio objects
+thumbnails = ['../media/contigo.jpg', '../media/waves.jpg']; // object storing paths for album covers and backgrounds
+songArtists = ['Ikson', 'Ikson']; // object storing track artists
+songTitles = ["Ikson - Contigo (Official)", "Ikson - Waves (Official)"]; // object storing track titles
+
+// function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
+let playing = true;
+function playPause() {
+    if (playing) {
+        const song = document.querySelector('#song'),
+        thumbnail = document.querySelector('#thumbnail');
+
+        pPause.src = "../media/pause.png"
+        thumbnail.style.transform = "scale(1.15)";
+        
+        song.play();
+        playing = false;
+    } else {
+        pPause.src = "../media/play.png"
+        thumbnail.style.transform = "scale(1)"
+        
+        song.pause();
+        playing = true;
+    }
+}
+
+// automatically play the next song at the end of the audio object's duration
+song.addEventListener('ended', function(){
+    nextSong();
+});
+
+// function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track 
+function nextSong() {
+    songIndex++;
+    if (songIndex > 1) {
+        songIndex = 0;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+
+// function where songIndex is decremented, song/thumbnail image/background image/song artist/song title changes to previous index value, and playPause() runs to play previous track 
+function previousSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = 1;
+    };
+    song.src = songs[songIndex];
+    thumbnail.src = thumbnails[songIndex];
+    background.src = thumbnails[songIndex];
+
+    songArtist.innerHTML = songArtists[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playPause();
+}
+
+// update progressBar.max to song object's duration, same for progressBar.value, update currentTime/duration DOM
+function updateProgressValue() {
+    progressBar.max = song.duration;
+    progressBar.value = song.currentTime;
+    document.querySelector('.currentTime').innerHTML = (formatTime(Math.floor(song.currentTime)));
+    if (document.querySelector('.durationTime').innerHTML === "NaN:NaN") {
+        document.querySelector('.durationTime').innerHTML = "0:00";
+    } else {
+        document.querySelector('.durationTime').innerHTML = (formatTime(Math.floor(song.duration)));
+    }
+};
+
+// convert song.currentTime and song.duration into MM:SS format
+function formatTime(seconds) {
+    let min = Math.floor((seconds / 60));
+    let sec = Math.floor(seconds - (min * 60));
+    if (sec < 10){ 
+        sec  = `0${sec}`;
+    };
+    return `${min}:${sec}`;
+};
+
+// run updateProgressValue function every 1/2 second to show change in progressBar and song.currentTime on the DOM
+setInterval(updateProgressValue, 500);
+
+// function where progressBar.value is changed when slider thumb is dragged without auto-playing audio
+function changeProgressBar() {
+    song.currentTime = progressBar.value;
+};*/
